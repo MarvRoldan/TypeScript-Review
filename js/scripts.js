e691fde362500f0c1dd4e3158ac1f2669480f100
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-06 14:05:00 -0600

    Added methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 var Calculator = /** @class */ (function () {
     function Calculator() {
+        // Properties:
+        this.form = document.querySelector("#calculator");
+        this.num1 = document.querySelector('[name="num1"]');
+        this.num2 = document.querySelector('[name="num2"]');
+        this.result = document.querySelector("#calculator strong");
     }
+    // Methods:
+    Calculator.prototype.add = function (num1, num2) {
+        return num1 + num2;
+    };
+    Calculator.prototype.subtract = function (num1, num2) {
+        return num1 - num2;
+    };
+    Calculator.prototype.multiply = function (num1, num2) {
+        return num1 * num2;
+    };
+    Calculator.prototype.divide = function (num1, num2) {
+        return num1 / num2;
+    };
+    Calculator.prototype.remainder = function (num1, num2) {
+        return num1 % num2;
+    };
     return Calculator;
 }());
--- src/ts/Calculator.Class.ts
+++ src/ts/Calculator.Class.ts
@@ -1,3 +1,23 @@
 class Calculator {
-
+  // Properties:
+  form: HTMLElement = document.querySelector("#calculator");
+  num1: HTMLInputElement = document.querySelector('[name="num1"]');
+  num2: HTMLInputElement = document.querySelector('[name="num2"]');
+  result: HTMLElement = document.querySelector("#calculator strong");
+  // Methods:
+  add(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+  subtract(num1: number, num2: number): number {
+    return num1 - num2;
+  }
+  multiply(num1: number, num2: number): number {
+    return num1 * num2;
+  }
+  divide(num1: number, num2: number): number {
+    return num1 / num2;
+  }
+  remainder(num1: number, num2: number): number {
+    return num1 % num2;
+  }
 }
