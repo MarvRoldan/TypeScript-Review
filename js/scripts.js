15f7faa4412b6646a23fdf04804c949d818f5f45
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-06 14:52:13 -0600

    Got calculator to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 var Calculator = /** @class */ (function () {
     function Calculator() {
         // Properties:
-        this.form = document.querySelector("#calculator");
+        this.form = document.querySelector('#calculator');
         this.num1 = document.querySelector('[name="num1"]');
         this.num2 = document.querySelector('[name="num2"]');
-        this.operator = document.querySelector('[name ="operator"]');
-        this.result = document.querySelector("#calculator strong");
+        this.operator = document.querySelector('[name="operator"]');
+        this.result = document.querySelector('#calculator strong');
     }
     // Methods:
     Calculator.prototype.add = function (num1, num2) {
@@ -29,18 +29,33 @@ var Calculator = /** @class */ (function () {
     Calculator.prototype.calculate = function () {
         var num1 = Number(this.num1.value);
         var num2 = Number(this.num2.value);
+        var answer = 0;
         switch (this.getOperator()) {
-            case "addition":
-                return this.add(num1, num2);
-            case "subtraction":
-                return this.subtract(num1, num2);
-            case "multiplication":
-                return this.multiply(num1, num2);
-            case "division":
-                return this.divide(num1, num2);
-            case "remainder":
-                return this.remainder(num1, num2);
+            case 'addition':
+                answer = this.add(num1, num2);
+                break;
+            case 'subtraction':
+                answer = this.subtract(num1, num2);
+                break;
+            case 'multiplication':
+                answer = this.multiply(num1, num2);
+                break;
+            case 'division':
+                answer = this.divide(num1, num2);
+                break;
+            case 'remainder':
+                answer = this.remainder(num1, num2);
+                break;
         }
+        this.result.textContent = answer.toString();
+        return answer;
     };
     return Calculator;
 }());
+// Create calculator from our class.
+var newCalculator = new Calculator();
+// Event listener.
+newCalculator.form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    newCalculator.calculate();
+});
