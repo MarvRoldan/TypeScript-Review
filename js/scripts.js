c2a534c72117c6f26a83da41ec330ffdbfc958b0
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-06 20:13:50 -0600

    Import fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,7 @@
-var Calculator = /** @class */ (function () {
-    function Calculator() {
-        // Properties:
-        this.form = document.querySelector('#calculator');
-        this.num1 = document.querySelector('[name="num1"]');
-        this.num2 = document.querySelector('[name="num2"]');
-        this.operator = document.querySelector('[name="operator"]');
-        this.result = document.querySelector('#calculator strong');
-    }
-    // Methods:
-    Calculator.prototype.add = function (num1, num2) {
-        return num1 + num2;
-    };
-    Calculator.prototype.subtract = function (num1, num2) {
-        return num1 - num2;
-    };
-    Calculator.prototype.multiply = function (num1, num2) {
-        return num1 * num2;
-    };
-    Calculator.prototype.divide = function (num1, num2) {
-        return num1 / num2;
-    };
-    Calculator.prototype.remainder = function (num1, num2) {
-        return num1 % num2;
-    };
-    Calculator.prototype.getOperator = function () {
-        return this.operator.value;
-    };
-    Calculator.prototype.calculate = function () {
-        var num1 = Number(this.num1.value);
-        var num2 = Number(this.num2.value);
-        var answer = 0;
-        switch (this.getOperator()) {
-            case 'addition':
-                answer = this.add(num1, num2);
-                break;
-            case 'subtraction':
-                answer = this.subtract(num1, num2);
-                break;
-            case 'multiplication':
-                answer = this.multiply(num1, num2);
-                break;
-            case 'division':
-                answer = this.divide(num1, num2);
-                break;
-            case 'remainder':
-                answer = this.remainder(num1, num2);
-                break;
-        }
-        this.result.textContent = answer.toString();
-        return answer;
-    };
-    return Calculator;
-}());
+/**
+ * Using a class (and DOM) in TypeScript.
+ */
+import { Calculator } from './Calculator.Class.js';
 // Create calculator from our class.
 var newCalculator = new Calculator();
 // Event listener.
@@ -59,3 +9,79 @@ newCalculator.form.addEventListener('submit', function (event) {
     event.preventDefault();
     newCalculator.calculate();
 });
+/**
+ * Enum Type.
+ * @link https://www.typescriptlang.org/docs/handbook/enums.html
+ * enums are a set of named constants!
+ */
+var Pizza;
+(function (Pizza) {
+    Pizza[Pizza["PEPPORONI"] = 1] = "PEPPORONI";
+    Pizza[Pizza["HAWAIIAN"] = 2] = "HAWAIIAN";
+    Pizza[Pizza["CANADIAN"] = 3] = "CANADIAN";
+    Pizza[Pizza["HAM"] = 4] = "HAM";
+})(Pizza || (Pizza = {}));
+console.log("\nWe have all kinds of pizzas!\nPizza 1 is: " + Pizza[1] + "\nPizza 2 is: " + Pizza[2] + "\nPizza 3 is: " + Pizza[3] + "\nPizza 4 is: " + Pizza[4] + "\n");
+/**
+ * Function.
+ * @link https://www.typescriptlang.org/docs/handbook/functions.html
+ */
+var todoForm = document.querySelector('#todo');
+var todoInput = document.querySelector('[name="todo"]');
+var todoList = document.querySelector('#todo ul');
+var addNewTodo = function (event) {
+    event.preventDefault();
+    var newLi = document.createElement('LI');
+    newLi.textContent = todoInput.value;
+    todoList.appendChild(newLi);
+};
+todoForm.addEventListener('submit', addNewTodo);
+/**
+ * Literal type example.
+ */
+var addOrSubtract = function (op, num1, num2) {
+    if (op == 'add')
+        return Number(num1 + num2);
+    else
+        return Number(num1 - num2);
+};
+// addOrSubtract( 'multiply', 3, 6 ); // Would error now!
+console.log(addOrSubtract('add', 3, 10)); // 13
+/**
+ * Record example.
+ * We are able to enforce the types within our data-structure!
+ * <keyType, valueType>
+ */
+var myRecord = {
+    'first_key': 'Hello, World!',
+    'name': 'Bill',
+    'nickname': 'Bob'
+};
+console.log(myRecord['first_key']);
+/**
+ * Loop example.
+ */
+var myArray = ['A', 'B', 'C'];
+for (var _i = 0, myArray_1 = myArray; _i < myArray_1.length; _i++) {
+    var value = myArray_1[_i];
+    console.log(value);
+}
+/**
+ * Functions calling functions.
+ */
+var originalString = 'mystring';
+var addStringToBeginning = function (string) {
+    return string + originalString;
+};
+var addStringToEnd = function (string) {
+    return originalString + string;
+};
+var addStringsToBothEnds = function () {
+    return function (beginningString, endingString) {
+        originalString = addStringToBeginning(beginningString);
+        originalString = addStringToEnd(endingString);
+        return originalString;
+    };
+};
+// Calling functions from functions, we need to use new parenthesis!
+addStringsToBothEnds()('HELLOBEGIN:', 'GOODBYEEND!');
--- src/ts/scripts.ts
+++ src/ts/scripts.ts
@@ -1,7 +1,108 @@
+/**
+ * Using a class (and DOM) in TypeScript.
+ */
+
+import { Calculator } from './Calculator.Class.js';
+
 // Create calculator from our class.
 const newCalculator: Calculator = new Calculator();
 // Event listener.
-newCalculator.form.addEventListener( 'submit', event => {
+newCalculator.form.addEventListener( 'submit', (event: Event): void => {
   event.preventDefault();
   newCalculator.calculate();
 } );
+
+/**
+ * Enum Type.
+ * @link https://www.typescriptlang.org/docs/handbook/enums.html
+ * enums are a set of named constants!
+ */
+
+enum Pizza { PEPPORONI=1, HAWAIIAN=2, CANADIAN=3, HAM=4 }
+
+console.log( `
+We have all kinds of pizzas!
+Pizza 1 is: ${Pizza[1]}
+Pizza 2 is: ${Pizza[2]}
+Pizza 3 is: ${Pizza[3]}
+Pizza 4 is: ${Pizza[4]}
+` );
+
+
+/**
+ * Function.
+ * @link https://www.typescriptlang.org/docs/handbook/functions.html
+ */
+
+const todoForm: HTMLElement = document.querySelector( '#todo' );
+const todoInput: HTMLInputElement = document.querySelector( '[name="todo"]' );
+const todoList: HTMLElement = document.querySelector( '#todo ul' );
+
+const addNewTodo = (event: Event): void => {
+  event.preventDefault();
+  const newLi: HTMLElement = document.createElement( 'LI' );
+  newLi.textContent = todoInput.value;
+  todoList.appendChild( newLi );
+}
+
+todoForm.addEventListener( 'submit', addNewTodo );
+
+
+/**
+ * Literal type example.
+ */
+
+const addOrSubtract = ( op: 'add' | 'subtract', num1: number, num2: number ): number => {
+  if ( op == 'add' ) return Number( num1 + num2 );
+  else return Number( num1 - num2 );
+}
+
+// addOrSubtract( 'multiply', 3, 6 ); // Would error now!
+console.log( addOrSubtract( 'add', 3, 10 ) ); // 13
+
+/**
+ * Record example.
+ * We are able to enforce the types within our data-structure!
+ * <keyType, valueType>
+ */
+
+const myRecord: Record<string, string> = {
+  'first_key': 'Hello, World!',
+  'name': 'Bill',
+  'nickname': 'Bob'
+};
+
+console.log( myRecord['first_key'] );
+
+/**
+ * Loop example.
+ */
+
+const myArray: Array<string> = [ 'A', 'B', 'C' ];
+for ( let value of myArray ) {
+  console.log( value );
+}
+
+/**
+ * Functions calling functions.
+ */
+
+let originalString = 'mystring';
+const addStringToBeginning = (string: string): string => {
+  return string + originalString;
+}
+
+const addStringToEnd = (string: string): string => {
+  return originalString + string;
+}
+
+const addStringsToBothEnds = (): Function => {
+  return ( beginningString: string, endingString: string ): string => {
+    originalString = addStringToBeginning( beginningString );
+    originalString = addStringToEnd( endingString );
+    return originalString;
+  };
+}
+
+// Calling functions from functions, we need to use new parenthesis!
+addStringsToBothEnds()( 'HELLOBEGIN:', 'GOODBYEEND!' );
